cde9ccffdf341cc51301b948bbc11c322012e8b7
Gonzalo Salinas <gsalinase91@gmail.com>
2022-06-01 22:40:32 -0400

    song and playlist added and migrated

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from '@prisma/client'
 import bcrypt from 'bcrypt'
-import { create } from 'lodash'
 import { artistsData } from './songsData'
 
 const prisma = new PrismaClient()
@@ -34,6 +33,23 @@ const run = async () => {
       password: bcrypt.hashSync('password', salt)
     }
   })
+
+  const songs = await prisma.song.findMany({})
+  await Promise.all(new Array(10).fill(1).map(async (_, i) => {
+    return prisma.playlist.create({
+      data: {
+        name: `Playlist #${i + 1}`,
+        user: {
+          connect: { id: user.id }
+        },
+        songs: {
+          connect: songs.map((song) => ({
+            id: song.id
+          }))
+        },
+      }
+    })
+  }))
 }
 
 run()
